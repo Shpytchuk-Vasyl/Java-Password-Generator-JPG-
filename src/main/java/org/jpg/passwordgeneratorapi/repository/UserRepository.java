37957f8d4a31339ccaefa74db4ce3ed679a557d0
Shpytchuk-Vasyl <vasyl.shpytchuk.pz.2022@lpnu.ua>
2024-02-08 19:50:00 +0200

    Add user repository

---
+++ src/main/java/org/jpg/passwordgeneratorapi/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.jpg.passwordgeneratorapi.repository;
+
+import org.jpg.passwordgeneratorapi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
