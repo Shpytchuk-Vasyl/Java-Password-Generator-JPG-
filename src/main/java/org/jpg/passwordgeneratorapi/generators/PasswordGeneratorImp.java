bcb26bfd416db8a2f72011c51f51fd353f0c7f68
Shpytchuk-Vasyl <vasyl.shpytchuk.pz.2022@lpnu.ua>
2024-02-08 21:18:37 +0200

    add password generator implementation

---
+++ src/main/java/org/jpg/passwordgeneratorapi/generators/PasswordGeneratorImp.java
@@ -0,0 +1,20 @@
+package org.jpg.passwordgeneratorapi.generators;
+
+import org.springframework.stereotype.Service;
+import java.util.Random;
+import java.util.stream.*;
+
+
+@Service
+public class PasswordGeneratorImp implements PasswordGenerator {
+    @Override
+    public String generate(String available, Integer size) {
+        Random random = new Random();
+        int stringSize = available.length();
+        return IntStream.range(0, size)
+                .parallel()
+                .map(i -> random.nextInt(0, stringSize))
+                .mapToObj(i -> available.charAt(i) + "")
+                .collect(Collectors.joining());
+    }
+}
