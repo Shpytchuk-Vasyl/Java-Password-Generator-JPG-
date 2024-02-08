50d43667fed04630642761b24d839521084c3d2a
Shpytchuk-Vasyl <vasyl.shpytchuk.pz.2022@lpnu.ua>
2024-02-08 21:33:12 +0200

    add user controller

---
+++ src/main/java/org/jpg/passwordgeneratorapi/controllers/UserController.java
@@ -0,0 +1,28 @@
+package org.jpg.passwordgeneratorapi.controllers;
+
+import org.jpg.passwordgeneratorapi.entity.User;
+import org.jpg.passwordgeneratorapi.exceptions.UserNotFoundException;
+import org.jpg.passwordgeneratorapi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/")
+public class UserController {
+
+    @Autowired
+    UserService service;
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) throws UserNotFoundException {
+        return ResponseEntity.ok(service.findUser(id));
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<User> registerUser(@RequestBody User user) throws UserNotFoundException {
+        return ResponseEntity.ok(service.registerUser(user));
+    }
+
+
+}
