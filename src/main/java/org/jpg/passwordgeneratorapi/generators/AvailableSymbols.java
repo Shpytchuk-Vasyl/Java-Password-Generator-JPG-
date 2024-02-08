18463a81eaecf1d5826e2495a290a348084a4f81
Shpytchuk-Vasyl <vasyl.shpytchuk.pz.2022@lpnu.ua>
2024-02-08 22:15:59 +0200

    add available symbols class

---
+++ src/main/java/org/jpg/passwordgeneratorapi/generators/AvailableSymbols.java
@@ -0,0 +1,27 @@
+package org.jpg.passwordgeneratorapi.generators;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.jpg.passwordgeneratorapi.exceptions.IllegalStateOfAvailableSymbols;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AvailableSymbols {
+
+    private Integer size;
+    private Boolean uppercase, lowercase, numbers, symbols;
+
+    public String getString() throws IllegalStateOfAvailableSymbols {
+        if((uppercase || lowercase || numbers ||symbols) == false)
+            throw new IllegalStateOfAvailableSymbols();
+        for (char i = 'A'; i < 'z'; i++) {
+            System.out.print(i);
+        }
+        return (uppercase ? "A-Z" : "") +
+                (lowercase ? "a-z" : "") +
+                (numbers ? "0123456789" : "") +
+                (symbols ? "?!;:.,_<>[]{}\\|/#@$%&`~^'₴*-+=" : "");
+    }
+}
