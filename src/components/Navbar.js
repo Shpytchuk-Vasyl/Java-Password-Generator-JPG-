9b817e2ce4da65fb20042bf36bf6e2973b8ea841
Shpytchuk-Vasyl <vasyl.shpytchuk.pz.2022@lpnu.ua>
2024-02-10 21:03:11 +0200

    add buttons to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,22 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
-import {click} from "@testing-library/user-event/dist/click";
-
+import {Button} from "./Button";
+import "./Navbar.css"
 function Navbar() {
     const [click,setClick] = useState(false)
+    const [button,setButton] = useState(true)
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    }
+    window.addEventListener("resize", showButton)
+    useEffect(() => {
+     showButton()
+    }, []);
 
     const handleClick = () => setClick(!click);
 
@@ -11,7 +24,7 @@ function Navbar() {
         <>
         <nav className="navbar">
             <div className="navbar-container">
-                <Link to="/" className="navbar-log">
+                <Link to="/" className="navbar-log" onClick={handleClick}>
                     JPG<i className="fa fa-lock"/>
                 </Link>
                 <div className="menu-icon" onClick={handleClick}>
@@ -24,7 +37,15 @@ function Navbar() {
                     <li className="nav-item">
                         <Link to="/profile" className="nav-links">Profile</Link>
                     </li>
+                    <li className="nav-item">
+                        <Link to="/sing up" className="nav-links-mobile">Sing up</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/sing up" className="nav-links-mobile">Log in</Link>
+                    </li>
                 </ul>
+                {button && <Button to="/sing up" buttonStyle="btn--outline">Sing Up</Button>}
+                {button && <Button to="/log in" buttonStyle="btn--outline">Log In</Button>}
             </div>
         </nav>
         </>
