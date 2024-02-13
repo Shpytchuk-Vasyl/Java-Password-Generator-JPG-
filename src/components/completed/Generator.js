46df02d84154317c3afcee5c20c0e2fa9ea2c1b8
Shpytchuk-Vasyl <vasyl.shpytchuk.pz.2022@lpnu.ua>
2024-02-13 10:04:20 +0200

    add checkbox style

--- src/components/completed/Generator.js
+++ src/components/completed/Generator.js
@@ -1,66 +1,128 @@
 import React, {useState} from 'react';
 import "./Generator.css"
 function Generator() {
-    const [reliability,setReliability] = useState("very strong")
-    const [passwordLength,setPasswordLength] = useState(50)
-    return (
-        <div className="generator-container" id="generator-container">
-            <h1>Java Password Generator</h1>
-            <p>Create strong and secure passwords to keep your account safe online.</p>
-            <div className="input-container">
-                <div className="input-controler">
-                    <div className="input-">
-                        <input className="password-input" readOnly/>
-                        <label className="reliability-label">{reliability}</label>
-                    </div>
-                    <i className="fa fa-copy"/>
-                </div>
-                <div className="button-wrapper">
-                    <button className="generate-button">Generate</button>
-                </div>
-            </div>
-            <div className="generator-setting-container">
-                <div className="generator-setting-length">
-                    <div className="setting-length-wrapper">
-                        <label><span>Characters used</span>: {passwordLength}</label>
-                    </div>
-                    <div className="slider-settings">
-                        <button className="button-circle">
-                            <i className="fa fa-minus"/>
-                        </button>
-                        <div className="slider-container">
-                            <input type="range" min="1" max="50" className="slider"
-                                  onInput={event => {{
-                                      var value = (event.target.value-event.target.min)/(event.target.max-event.target.min)*100
-                                      event.target.style.background = 'linear-gradient(to right, #82CFD0 0%, #82CFD0 ' + value + '%, #fff ' + value + '%, white 100%)'
-                                      setPasswordLength(event.target.value)
-                                  }}}
-                                />
-                            <div className="slider-border-part"
-                                 //style="--slider-border-part-width: 28.571428571428573%;"
-                            ></div>
+    const [reliability, setReliability] = useState("very strong")
+    const [checkboxStates, setCheckboxStates] = useState({
+            lowercase: true,
+            uppercase: true,
+            numbers: true,
+            symbols: true,
+        })
+    const handleCheckboxChange = (checkboxName) => {
+            setCheckboxStates(prevState => {
+                const updatedStates = {
+                    ...Object.fromEntries(
+                        Object.entries(prevState).map(([name, value]) => [name, name === checkboxName ? !value : value])
+                    ),
+                };
+
+                const activeCheckboxes = Object.values(prevState).filter(value => value);
+                if (activeCheckboxes.length === 1 && prevState[checkboxName]) {
+                    return prevState;
+                }
+
+                return updatedStates;
+            });
+        };
+    const  [sliderLength, setSliderLength = (length) => {
+
+    }] = useState({
+        min: 1,
+        max: 50,
+        value: 50,
+        sliderValue: 100
+    })
+    const updateSliderLength = (length) => {
+        setSliderLength(prevState => ({
+            ...prevState,
+            value: length,
+            sliderValue: (length - 1 - prevState.min) / (prevState.max - 2 - prevState.min) * 100
+        }));
+    };
+
+    const updateSlider = (newLength) => {
+        updateSliderLength(newLength)
+        document.getElementById("slider").style.background = 'linear-gradient(to right, #82CFD0 0%, #82CFD0 ' + sliderLength.sliderValue + '%, #fff ' + sliderLength.sliderValue + '%, white 100%)'
+        //document.getElementById("slider").value = newLength
+    }
+
+
+
+        return (
+            <div className="generator-container" id="generator-container">
+                <h1>Java Password Generator</h1>
+                <p>Create strong and secure passwords to keep your account safe online.</p>
+                <div className="input-container">
+                    <div className="input-controler">
+                        <div className="input-">
+                            <input className="password-input" readOnly/>
+                            <label className="reliability-label">{reliability}</label>
                         </div>
-                        <button className="button-circle">
-                            <i className="fa fa-plus"/>
-                        </button>
+                        <i className="fa fa-copy"/>
+                    </div>
+                    <div className="button-wrapper">
+                        <button className="generate-button">Generate</button>
                     </div>
                 </div>
-                <div className="generator-setting-complexity">
-                    <div className="complexity-checkbox">
-                        <label>abc</label>
-                        <input type="checkbox" className="checkbox-input lowercase"/>
-                        <label>ABC</label>
-                        <input type="checkbox" className="checkbox-input upperrcase"/>
-                        <label>0-9</label>
-                        <input type="checkbox" className="checkbox-input numbers"/>
-                        <label>%-?</label>
-                        <input type="checkbox" className="checkbox-input symbols"/>
+                <div className="generator-setting-container">
+                    <div className="generator-setting-length">
+                        <div className="setting-length-wrapper">
+                            <label><span>Characters used</span>: {sliderLength.value}</label>
+                        </div>
+                        <div className="slider-settings">
+                            <button className="button-circle" onClick={e => updateSlider(Math.max(sliderLength.value - 1, sliderLength.min))}>
+                                <i className="fa fa-minus"/>
+                            </button>
+                            <div className="slider-container">
+                                <input type="range" min={sliderLength.min} max={sliderLength.max} className="slider" id="slider"
+                                       onInput={event => {
+                                           updateSlider(event.target.value)
+                                       }}
+                                       onClick={event => {
+                                           updateSlider(event.target.value)
+                                       }}
+                                />
+                                <div className="slider-border-part"></div>
+                            </div>
+                            <button className="button-circle" onClick={e=>updateSlider(Math.min(sliderLength.value + 1, sliderLength.max))}>
+                                <i className="fa fa-plus"/>
+                            </button>
+                        </div>
+                    </div>
+                    <div className="generator-setting-complexity">
+                        <div className="complexity-checkbox">
+                            <label>abc</label>
+                            <input
+                                type="checkbox"
+                                className="checkbox-input lowercase"
+                                checked={checkboxStates.lowercase}
+                                onChange={() => handleCheckboxChange('lowercase')}
+                            />
+                            <label>ABC</label>
+                            <input
+                                type="checkbox"
+                                className="checkbox-input uppercase"
+                                checked={checkboxStates.uppercase}
+                                onChange={() => handleCheckboxChange('uppercase')}
+                            />
+                            <label>0-9</label>
+                            <input
+                                type="checkbox"
+                                className="checkbox-input numbers"
+                                checked={checkboxStates.numbers}
+                                onChange={() => handleCheckboxChange('numbers')}
+                            />
+                            <label>%-?</label>
+                            <input
+                                type="checkbox"
+                                className="checkbox-input symbols"
+                                checked={checkboxStates.symbols}
+                                onChange={() => handleCheckboxChange('symbols')}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
-}
-
-
-export default Generator;
+        );
+    }
+    export default Generator;
