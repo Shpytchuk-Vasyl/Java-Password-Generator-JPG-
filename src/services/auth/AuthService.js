870aac53376bf45ddbab0249d5759683d76ac8f2
Shpytchuk-Vasyl <vasyl.shpytchuk.pz.2022@lpnu.ua>
2024-02-14 11:56:51 +0200

    add auth service

---
+++ src/services/auth/AuthService.js
@@ -0,0 +1,44 @@
+import axios from "axios"
+
+const API_URL = "http://localhost:8080/api/v1/auth/"
+class AuthService {
+    login(username, password, badRequestHandle) {
+        return axios.post(API_URL + "login", {username,password}).then(
+            resource => {
+                if(resource.status === 200) {
+                    localStorage.setItem("user", resource.data)
+                } else {
+                    badRequestHandle(resource.status, resource.data)
+                }
+                return resource.data
+            }
+        )
+    }
+
+
+    signup(username, password, badRequestHandle ) {
+        return axios.post(API_URL + "signup", {username,password}).then(
+            resource => {
+                if(resource.status === 200) {
+                    localStorage.setItem("user", resource.data)
+                } else {
+                    badRequestHandle(resource.status, resource.data)
+                }
+                return resource.data
+            }
+        )
+    }
+
+    logout() {
+        localStorage.removeItem("user")
+    }
+
+    getUser() {
+        const userObject = { user: "tru" };
+        localStorage.setItem("user", JSON.stringify(userObject))
+        return JSON.parse(localStorage.getItem("user")).user
+    }
+
+}
+
+export default new AuthService()
