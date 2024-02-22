ceea07acb6d0bcb391faeba838438ce465e80767
Shpytchuk-Vasyl <vasyl.shpytchuk.pz.2022@lpnu.ua>
2024-02-22 19:38:01 +0200

    add auth controller for registration with google

--- src/main/java/org/jpg/passwordgeneratorapi/controllers/AuthController.java
+++ src/main/java/org/jpg/passwordgeneratorapi/controllers/AuthController.java
@@ -11,17 +11,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Collection;
 
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("api/v1/auth")
 public class AuthController {
@@ -38,20 +34,21 @@ public class AuthController {
     JwtUtil jwtUtil;
 
     @PostMapping("/login")
-    public ResponseEntity<?> authenticateUser(@Valid @RequestBody User user) throws UserNotFoundException, IncorrectPasswordException, UserRegisteredWithGoogleException {
+    public ResponseEntity<JwtResponse> authenticateUser(@Valid @RequestBody User user) throws UserNotFoundException, IncorrectPasswordException, UserRegisteredWithGoogleException {
         User login = userService.login(user);
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String jwt = jwtUtil.generateJwtToken(authentication);
 
-        login.setPassword(null);
+        login.setPassword("");
         return ResponseEntity.ok(new JwtResponse(jwt, login));
 
     }
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@Valid  @RequestBody User user) throws UserIsAlreadyRegistered, IncorrectPasswordException, UserRegisteredWithGoogleException, UserNotFoundException {
+        if(user.getPassword().isBlank()) throw new IncorrectPasswordException("Password can't be empty");
         userService.registerUser(User.builder()
                 .email(user.getEmail())
                 .password(user.getPassword())
@@ -59,5 +56,37 @@ public class AuthController {
         return authenticateUser(user);
     }
 
+
+}
+
+@RestController
+class GoogleAuth {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    JwtUtil jwtUtil;
+
+
+    @GetMapping("/login/oauth2/code/google/")
+    public ResponseEntity<?> signInWithGoogle() throws UserNotFoundException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null) {
+            authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            ((OAuth2AuthenticatedPrincipal)authentication.getPrincipal())
+                                    .getAttribute("email").toString(), ""));
+            String jwt = jwtUtil.generateJwtToken(authentication);
+            return ResponseEntity.ok(new JwtResponse(jwt, userService.findUser(authentication.getName())));
+        }
+        return ResponseEntity.badRequest().build();
+    }
 
 }
