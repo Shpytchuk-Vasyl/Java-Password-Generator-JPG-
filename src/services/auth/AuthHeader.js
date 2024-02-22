8668a851e3f98f7a95fb259273313586687fb01a
Shpytchuk-Vasyl <vasyl.shpytchuk.pz.2022@lpnu.ua>
2024-02-22 20:38:34 +0200

    add cors allow headers

--- src/services/auth/AuthHeader.js
+++ src/services/auth/AuthHeader.js
@@ -1,6 +1,13 @@
 export default function authHeader() {
     const userAndToken = JSON.parse(localStorage.getItem("user"))
     return userAndToken && userAndToken.jwtToken
-        ? {Authorization: "Bearer " + userAndToken.jwtToken, "Content-Type": "application/json"}
-        : {}
+        ? {Authorization: "Bearer " + userAndToken.jwtToken,
+            "Content-Type": "application/json",
+            'Access-Control-Allow-Origin' : '*',
+            'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+        }
+        : {"Content-Type": "application/json",
+            'Access-Control-Allow-Origin' : '*',
+            'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+        }
 }
