c8d83e26e1a418e39b44f46dec37035a793d2b2a
Shpytchuk-Vasyl <vasyl.shpytchuk.pz.2022@lpnu.ua>
2024-02-22 20:39:23 +0200

    add registration with google function

--- src/services/auth/AuthService.js
+++ src/services/auth/AuthService.js
@@ -1,4 +1,6 @@
 import axios from "axios"
+import UserService from "../UserService";
+import AuthHeader from "./AuthHeader";
 
 const API_URL = "http://localhost:8080/api/v1/auth/"
 class AuthService {
@@ -8,15 +10,17 @@ class AuthService {
 
     sign(email, password, RequestHandle, endPoint) {
         axios.post(API_URL + endPoint, {
+            "id": 0,
             "email": email,
             "password": password
-        })
+        },{headers: AuthHeader()})
             .then(
                 resource => {
                     localStorage.setItem("user", JSON.stringify(resource.data))
                     RequestHandle(resource.status, resource.data)
                 }
             )
+            .catch(error => {UserService.errorHandle(error,RequestHandle)})
     }
 
 
@@ -24,16 +28,44 @@ class AuthService {
         this.sign(email, password, RequestHandle,"signup")
     }
 
+
+    loginWithGoogle(RequestHandle) {
+        //window.location.href = "http://localhost:8080";
+        //window.location.href = "http://localhost:8080/oauth2/authorization/google/?redirect_uri=http://localhost:3000/oauth2/callback/";
+        RequestHandle(404, "This function not available yet")
+
+        // axios.get("http://localhost:8080/oauth2/authorization/google", {headers: AuthHeader(), withCredentials: true})
+        //     .then(
+        //         resource => {
+        //             if(resource.redirected) console.log(resource,"redirect", resource.redirected);
+        //             localStorage.setItem("user", JSON.stringify(resource.data))
+        //             RequestHandle(resource.status, resource.data)
+        //         }
+        //     )
+        //     .catch(error => {
+        //         if(error.redirected) console.log(error,"redirect", error.redirected);
+        //         UserService.errorHandle(error, RequestHandle)
+        //     })
+
+    }
+
+
     logout() {
 
         localStorage.removeItem("user")
     }
 
     getUser() {
-        //localStorage.setItem("user", null)
-        let info = JSON.parse(localStorage.getItem("user"))
-        return info === null ? null : info.user
+        try {
+            let info = JSON.parse(localStorage.getItem("user"))
+            return info ? null : info.user
+        } catch (e) {
+            return null;
+        }
     }
+
+
+
 
 }
 
